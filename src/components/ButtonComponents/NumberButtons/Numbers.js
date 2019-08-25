eb5f33e30898b9249fad6754fb3fc2c546a8cebb
Jesse-Tingle <jtingle0@gmail.com>
2019-08-25 00:39:35 -0400

    create number buttons

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import any components needed
+import NumberButton from "./NumberButton";
 
 //Import your array data to from the provided data file
+import { numbers } from "../../../data";
+
+// import { tsPropertySignature } from "@babel/types";
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [buttonNumbers, setButtonNumbers] = useState(numbers);
+
   return (
     <div>
+
+      {buttonNumbers.map(number => <NumberButton key={number} text={ number }/>)}
+
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component */}
     </div>
   );
 };
+
+export default Numbers;
