684e28dae6efd55e7df956ddaaa6299799ce01a5
Jesse-Tingle <jtingle0@gmail.com>
2019-08-25 21:04:21 -0400

    installed SASS, created SASS file for App.scss, styled background color, display, and number buttons border-radius

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NumberButton = (props) => {
   return (
-    <button onClick={() => props.addNumber(props.text)}>
+    <button className="number-buttons" onClick={() => props.addNumber(props.text)}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     
       {/* <button onClick={() => props.add(props.text)} className="btn">{ props.text }</button> */}
